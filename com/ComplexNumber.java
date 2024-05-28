18d9a73cd9ba406a0813f56aab5502682075a3f7
D4tLe <datlex3@gmail.com>
2024-05-28 23:13:59 +0700

    refactor: update some methods

--- com/ComplexNumber.java
+++ com/ComplexNumber.java
@@ -69,6 +69,9 @@ public class ComplexNumber {
     // Override toString() method for a human-readable representation
     @Override
     public String toString() {
+        if (imag < 0) {
+            return real + "  -  " + (-imag) + "i";
+        }
         return real + "  +  " + imag + "i";
     }  
 
